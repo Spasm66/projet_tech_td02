aebac855e7b42f630d3f4bed1458fbf745ede0e7
Vivenza <vivinza@proton.me>
2025-09-23 12:05:30 +0200

    thibaut

--- game_text.c
+++ game_text.c
@@ -9,6 +9,59 @@ int main(void) {
     g = game_default();
     game_set_square(g, 1, 0, 1);
     game_print(g);
-    game_delete(g);
+    char user_sres = '\0';
+    while(!game_won(g))
+    {
+        scanf(" %c", &user_sres);
+        printf("input: %c\n", user_sres);
+        switch(user_sres)
+        {
+            case 't':
+                int posx = 0;
+                int posy = 0;
+                scanf(" %d", &posx);
+                scanf(" %d", &posy);
+                printf("posx: %d, posy: %d\n", posx, posy);
+                game_play_move(g, posx, posy, TENT);
+                break;
+
+            case 'g':
+                int posx = 0;
+                int posy = 0;
+                scanf(" %d", &posx);
+                scanf(" %d", &posy);
+                printf("posx: %d, posy: %d\n", posx, posy);
+                game_play_move(g, posx, posy, GRASS);
+                break;
+
+            case 'e':
+                int posx = 0;
+                int posy = 0;
+                scanf(" %d", &posx);
+                scanf(" %d", &posy);
+                printf("posx: %d, posy: %d\n", posx, posy);
+                game_play_move(g, posx, posy, EMPTY);
+                break;
+
+            case 'h':
+                printf("help\n");
+                break;
+
+            case 'r':
+                // reset
+                break;
+
+            case 'f':
+                // remplir d'herbe
+                break;
+
+            case 'q':
+                game_delete(g);
+                break;  
+                
+            default:
+                break;
+        }
+    }
     return EXIT_SUCCESS; 
 }
